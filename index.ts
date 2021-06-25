72fe222760d4aadaa022ca2edaeb768934c7a78e
huangshan@coocaa.com <312720804>
2021-06-25 19:36:23 +0800

    first commit : ts类型、ts函数

---
+++ index.ts
@@ -0,0 +1,156 @@
+// 3.1、函数的定义
+
+    // ts 中定义函数的方法
+
+    // 函数声明法
+        function run():string {
+            return 'run'
+        }
+
+        // 错误写法
+        // function run():string {
+        //     return 123
+        // }
+
+
+    // 匿名函数
+
+        var fun2 = function():number {
+            return 12
+        }
+
+        fun2() // 调用方法
+
+    
+    // ts 中定义方法传参
+        // function getInfo(name:string, age:number):string {
+        //     return `${name} --- ${age}`
+        // }
+
+    // 没有返回值的方法
+        // function run():void{
+        //     console.log('run')
+        // }
+        // run()
+
+  
+
+// 3.2 方法可选参数
+
+    // es5 里面方法的实参和形参可以不一样，但是在 ts 中必须一样，如果不一样就需要配置可选参数
+    // var getInfo = function(name:string, age?:number):string {
+    //     if (age) {
+    //         return `${name} --- ${age}`
+    //     } else {
+    //         return `${name} --- 年龄保密`
+    //     }
+    // }
+    // alert(getInfo('aa', 15))
+
+    // 注意：可选参数必须配置到参数的最后面
+
+    // 错误写法：
+    // var getInfo = function(name?:string, age:number):string {
+    //     if (age) {
+    //         return `${name} --- ${age}`
+    //     } else {
+    //         return `${name} --- 年龄保密`
+    //     }
+    // }
+    // alert(getInfo('aa', 15))
+
+// 3.3 默认参数 可选参数
+
+    // es5 里面没法设置默认参数， es6 和 ts 中都可以设置默认参数
+
+    // var getInfo = function(name:string, age:number = 20):string {
+    //     if (age) {
+    //         return `${name} --- ${age}`
+    //     } else {
+    //         return `${name} --- 年龄保密`
+    //     }
+    // }
+    // alert(getInfo('aa'))
+    
+
+
+// 3.4 剩余参数
+
+
+    // 三点运算符，接收新参传过来的值
+    // function sum (...result:number[]):number {
+    //     var sum = 0
+    //     for (var i = 0; i < result.length; i++) {
+    //         sum += result[i]
+    //     }
+    //     return sum
+    // }
+
+    // alert(sum(1, 2, 3, 7))
+
+
+    // function sum (a:number, b:number, ...result:number[]):number {
+    //     var sum = a+b
+    //     for (var i = 0; i < result.length; i++) {
+    //         sum += result[i]
+    //     }
+    //     return sum
+    // }
+
+    // alert(sum(1, 2, 3, 7))
+
+
+
+// 3.5 ts 函数重载
+
+    // Java 中方法的重载，重载指的是两个或者两个以上的同名函数，但他们的参数不一样，这是会出现函数重载的情况。
+
+    // typeScript 中的重载：通过为同一个函数提供多个函数类型定义来试下多种功能的母的
+
+    // ts 为了兼容 es5 以及 es6 重载的写法和 java 中有区别
+
+
+
+    // es 5中出现同名方法，下面的方法会替换上面的方法
+    // function css(config) {
+
+    // }
+
+    // function css(config, value) {
+
+    // }
+
+
+
+    // ts 中的重载
+    // function getInfo(name:string):string;
+
+    // function getInfo(age:number):number;
+
+    // function getInfo(str:any):any {
+    //     if (typeof str === 'string') {
+    //         return str
+    //     } else {
+    //         return str
+    //     }
+    // }
+
+    // alert(getInfo(123))
+
+
+
+// 3.6 箭头函数 es6 
+
+    // setTimeout(function() {
+    //     alert('run')
+    // }, 1000)
+
+
+//  箭头函数的 this 指向上下文
+    setTimeout(() => {
+        alert('run')
+    }, 1000)
+    
+
+
+   
---
+++ js/index(ts类型).js
@@ -0,0 +1,69 @@
+"use strict";
+/*
+TS 中的数据数据类型
+    布尔类型(boolean)
+    数字类型(number)
+    字符串类型(string)
+    数组类型(array)
+    元祖类型(tuple)
+    美剧类型(enum)
+
+    任意类型(any)
+    null 和 undefined
+    void 类型
+    never 类型
+*/
+// let str:string = '你好 ts'
+// let str1:string = '你好 4564fsgdh'
+// 数组类型（array） ts中定义数组有两种方式
+// es5 var arr = [1, '12314', false]
+// // 1.第一种定义数组的方式
+var arr = [1, 2, 3, 434, 5];
+var arr1 = ['12', '22'];
+// // 2.第二种定义数组的方式
+var arr2 = [11, 22, 33, 44, 55];
+// console.log(arr2)
+// 3.第二种定义数组的方式
+var arr3 = ['12314', 123, false];
+console.log(arr3);
+// 元祖类型（tuple） 属于数组的一种
+// let arr3:[string, number, boolean] = ['ts', 3, true]
+// 枚举类型 （enum）
+// enum Flag {success=1, error=-1}
+// var f:Flag = Flag.error
+// console.log(f)
+// enum Color {red, blue=5, orange}
+// var c:Color = Color.orange
+// console.log(c) // 输出 1
+// 任意类型（any）
+// var num:any = 123
+// num = '123'
+// num = true
+// console.log(num)
+// // 任意类型的用处
+// var oBox:any = document.getElementById('box')
+// oBox.style.color = 'blue'
+// var num:number;
+// console.log(num) // 输出： undefined  报错
+// var num:undefined | number
+// // num = 123
+// console.log(num)
+// var num:null
+// num = 2
+// console.log(num)
+var num;
+num = 124;
+console.log(num);
+// void类型： typescript 中的 void 代表没有任何类型，一般用于定义方法的时候没有返回值
+// function run():number {
+//     console.log('run')
+//     return 1
+// }
+// run()
+// never 类型: 是其他类型 （包括 null 和 undefined） 的子类型， 代表从不出现的值
+// 这意味着声明 never 的变量智能被 never 类型所赋值
+var a;
+// a= 123
+a = (function () {
+    throw new Error('错误');
+})();
